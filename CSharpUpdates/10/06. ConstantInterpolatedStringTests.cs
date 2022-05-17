5409808ffac7748d2fa800b073a46b4e0bf20e18
Mark Michaelis <mark@IntelliTect.com>
2022-05-17 14:01:24 -0700

    Added "Constant Interpolated Strings" start

---
+++ CSharpUpdates/10/06. ConstantInterpolatedStringTests.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EssentialCSharp.v10.Tests;
+
+[TestClass]
+public class ConstantInterpolatedStringTests
+{
+
+}
