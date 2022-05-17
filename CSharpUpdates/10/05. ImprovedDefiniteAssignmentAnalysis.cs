3db714e30c9fac27584d0fc17e92fd5278ae1f79
Mark Michaelis <mark@IntelliTect.com>
2022-05-17 13:45:09 -0700

    Added "Improved Definite Assignment Analysis" start

---
+++ CSharpUpdates/10/05. ImprovedDefiniteAssignmentAnalysis.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EssentialCSharp10.Tests;
+
+public static class StringNormalizationExtensions
+{
+    public static bool TryIntParse(this string text, out int number) =>
+        int.TryParse(text, out number);
+}
+
+[TestClass]
+public class ImprovedDefiniteAssignmentAnalysisTests
+{
+    [TestMethod]
+    public void TryIntParse_WithNullCoalesceOperator_Compiles()
+    {
+        string text = "forty-two";
+        if (text?.TryIntParse(out int number) == true)
+        {
+            number.ToString(); // Undefined CS0165 no longer occurs
+        }
+    }
+}
