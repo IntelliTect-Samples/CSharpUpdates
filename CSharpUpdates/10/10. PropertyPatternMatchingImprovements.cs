fb67384bb51832ba71bec0682ff5066ded67f71f
Mark Michaelis <mark@IntelliTect.com>
2022-05-17 15:00:12 -0700

    Added "Property Pattern Matching Improvements" start

---
+++ CSharpUpdates/10/10. PropertyPatternMatchingImprovements.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EssentialCSharp10.Tests;
+
+[TestClass]
+public class PropertyPatternMatchingImprovements
+{
+    public record LineItem(Guid ProductId, double Quantity) { }
+    public record Order(
+        int OrderNumber, string CustomerName, List<LineItem> Items) {}
+
+    // Complete assignment below before present C# 10 syntax.
+
+    record class Person(string Name){}
+    static Person AssertValidPerson(Person person)
+    {
+        // Assignment
+        // Check that Person name is not null or zero length using
+        // C# 8.0 pattern matching.
+        if(person == null)
+        {
+            throw new InvalidOperationException(
+                @$"Invalid {
+                    nameof(Person.Name)}.");
+        }
+        return person;
+    }
+
+    [TestMethod]
+    public void PropertyPatternMatching_GivenPersonWithValidName_Success()
+    {
+        Person person = new("Inigo Montoya");
+
+        AssertValidPerson(person);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void PropertyPatternMatching_GivenPersonWithEmptyName_Trhow()
+    {
+        Person person = new("");
+
+        AssertValidPerson(person);
+    }
+
+}
