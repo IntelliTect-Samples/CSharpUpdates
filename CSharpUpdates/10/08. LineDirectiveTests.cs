9a8cef1d6f29a9535d791faffcaa43d1d03300c0
Mark Michaelis <mark@IntelliTect.com>
2022-05-17 14:19:21 -0700

    Added "Enhanced Line Directive" start

---
+++ CSharpUpdates/10/08. LineDirectiveTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EssentialCSharp.v10.Tests;
+
+[TestClass]
+public class LineDirectiveTests
+{
+    [TestMethod]
+    public void LineDirective_ThrowException_LineAdjusted()
+    {
+    }
+}
